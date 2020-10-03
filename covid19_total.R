7e3f949f64c56008f7ac4cd83ede9f107fc81969
yueli711 <40526984+yueli711@users.noreply.github.com>
2020-10-03 15:51:48 -0400

    Update covid19_total.R

--- covid19_total.R
+++ covid19_total.R
@@ -5,6 +5,7 @@ library(data.table)
 library(DESeq2)
 library(DESeq)
 library(limma)
+library(pamr)
 suppressMessages(require(Seurat))
 suppressMessages(require(ggplot2))
 suppressMessages(require(cowplot))
@@ -12,6 +13,78 @@ suppressMessages(require(scater))
 suppressMessages(require(scran))
 suppressMessages(require(BiocParallel))
 suppressMessages(require(BiocNeighbors))
+
+setwd("/home/li/covid19/result01/total")
+cts<-read.table("gse147507_counts",head=TRUE)
+write.csv(cts,"gse147507_counts.csv")
+#setup DESeqDataSetFromMatrix
+countData<-cts
+condition <- factor(c(rep("mock",20),rep("CoV",20)), levels = c("mock","CoV"))
+coldata<-data.frame(row.names=colnames(countData),condition)
+condition
+coldata
+dds<-DESeqDataSetFromMatrix(countData=countData, colData=coldata, design=~condition)
+head(dds)
+featureData <- data.frame(gene=rownames(cts))
+mcols(dds) <- DataFrame(mcols(dds), featureData)
+mcols(dds)
+keep <- rowSums(counts(dds)) >= 10#keep sum counts >=10
+dds <- dds[keep,]
+dim(dds)
+dds$condition <- factor(dds$condition, levels = c("mock","CoV"))
+dds$condition <- relevel(dds$condition, ref = "mock")
+dds$condition <- droplevels(dds$condition)
+dds <- estimateSizeFactors(dds)
+dds_norm<-counts(dds,normalized=TRUE)
+head(dds_norm)
+write.csv(dds_norm,"gse147507_norm.csv")
+
+#remove batch effect_36celllines
+library(sva) #blog.sina.com.cn/s/blog_70a5f5210102wibx.html
+cdata<-read.csv("gse147507_norm_36celllines.csv", header = T,  row.names = 1)
+cdata_0<-cdata[apply(cdata==0,1,sum)!=ncol(cdata),]
+dim(cdata_0)
+cdata_0 <- as.matrix(cdata_0)
+csif <- read.table("pheno_gse147507_1_2_5_6_7_16.txt", header = T, row.names = 1)
+modcombat = model.matrix(~1, data = csif)
+batch = csif$batch
+combat_edata = ComBat(dat=cdata_0, batch=batch, mod=modcombat, par.prior=TRUE, prior.plots=F)
+write.table(combat_edata, "ComBat_data(all)(counts).txt_36", sep = "\t", quote = F)
+
+#remove batch effect_24celllines
+cdata <- read.table("gse147507_from40_5_6_7_16_norm.txt", header = T,  row.names = 1)
+cdata_0<-cdata[apply(cdata==0,1,sum)!=ncol(cdata),]
+dim(cdata_0)
+cdata_0 <- as.matrix(cdata_0)
+csif <- read.table("pheno_gse147507_5_6_7_16.txt", header = T, row.names = 1)
+modcombat = model.matrix(~1, data = csif)
+batch = csif$batch
+combat_edata = ComBat(dat=cdata_0, batch=batch, mod=modcombat, par.prior=TRUE, prior.plots=F)
+write.table(combat_edata, "ComBat_data(all)(counts).txt_24", sep = "\t", quote = F)
+
+#volcano plot
+cts<-read.table("gse147507_from40_5_6_7_16_norm.txt",head=TRUE)
+countData<-round(cts)
+condition <- factor(c(rep("mock",12),rep("CoV",12)), levels = c("mock","CoV"))
+#coldata<-data.frame(row.names=colnames(countData),condition)
+condition
+#coldata
+featureData <- data.frame(gene=rownames(cts))
+name01<-read.table("name.txt",header=FALSE)
+name02<-factor(c(t(name01)))
+coldata<-data.frame(name=name02, condition)
+coldata
+dds<-DESeqDataSetFromMatrix(countData=countData,colData=coldata, design=~condition)
+
+dds2<-DESeq(dds)
+resultsNames(dds2)
+res <- results(dds2)
+res
+res <- results(dds2, name="condition_CoV_vs_mock")
+res <- results(dds2, contrast=c("condition","CoV","mock"))
+resultsNames(dds2)
+write.csv(res,"gse147507_deg.csv")
+
 #generate 25 gene expression signature
 setwd("/home/li/covid19/result01/total")
 cell24_4<-read.table("24celllines_4patients_norm.txt")
